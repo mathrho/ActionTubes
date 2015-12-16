f11eda516b7303b8c6b8f19ea88e30ac69efecbf
mathrho <author@corpmail.com>
2015-12-16 13:15:02 +0100

    add skip when computing flow

--- extractOpticalFlow.m
+++ extractOpticalFlow.m
@@ -1,12 +1,14 @@
-function [] = extractOpticalFlow(vid_list)
+function [] = extractOpticalFlow(vid_list, fps)
 
 load_dir = '/home/zhenyang//Workspace/data/UCF11/frames';
-save_dir = '/home/zhenyang//Workspace/data/UCF11/features/flow_brox';
+save_dir = sprintf('/home/zhenyang//Workspace/data/UCF11/features/flow_brox@fps%d', fps);
 vid_list_file = '/home/zhenyang/Workspace/data/UCF11/list_traintest.txt';
 
 folderlist = dir(load_dir);
 foldername = {folderlist(:).name};
 foldername = setdiff(foldername,{'.','..'});
+
+skip = int32(30.0/fps)
 
 for i = vid_list
     if ~exist(fullfile(save_dir,foldername{i}),'dir')
@@ -14,13 +16,13 @@ for i = vid_list
     end
 
     filelist = dir(fullfile(load_dir,foldername{i},'*.jpg'));
-    for j = 1:(length(filelist)-1)
+    for j = 1:(length(filelist)-skip)
         im1 = imread(fullfile(load_dir,foldername{i},sprintf('frame-%06d.jpg',j)));
-        im2 = imread(fullfile(load_dir,foldername{i},sprintf('frame-%06d.jpg',j+1)));
+        im2 = imread(fullfile(load_dir,foldername{i},sprintf('frame-%06d.jpg',j+skip)));
         flow_img = compute_flow(im1, im2);
 
         imwrite(flow_img, fullfile(save_dir,foldername{i},sprintf('flow-%06d.png',j)));
 	end
-    i
+    disp(num2str(i));
 end
 end
